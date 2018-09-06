55d0f955464ef3251b8d2d0cf43b807a4f67e35e
ew0k <jakebrown188@gmail.com>
2018-09-06 21:42:50 +0000

    grab total HDD size

--- bash-collect-system-information.sh
+++ bash-collect-system-information.sh
@@ -24,7 +24,10 @@ main() {
     echo "CPU Info: $cpu_info"
     
     ram_size=$(grep MemTotal /proc/meminfo | awk '{print $2/(1024*1024) " GB"}')
-    echo "RAM size: $ram_size"
+    echo "RAM Size: $ram_size"
+
+    total_hdd_size=$(lsblk --output SIZE -n | head -n 1 | cut -c 2-)
+    echo "HDD Size: $total_hdd_size"
 }
 
 main
