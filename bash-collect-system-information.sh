1390238fd675981b72d4c92ba3cbf4987f766106
ew0k <jakebrown188@gmail.com>
2018-09-06 15:21:18 +0000

    grab date and uptime

--- bash-collect-system-information.sh
+++ bash-collect-system-information.sh
@@ -1,1 +1,21 @@
 #! /bin/bash
+
+# get-date() {
+#     local current_time=$(date +%H:%M:%S\ %Z)
+#     echo "$current_time"
+# }
+# 
+# get-uptime() {
+#     local uptime=$(uptime --pretty)
+#     echo "$uptime"
+# }
+
+main() {
+    current_time=$(date +%H:%M:%S\ %Z)
+    echo "Current Time: $current_time"
+
+    uptime=$(uptime --pretty)
+    echo "Uptime: $uptime"
+}
+
+main
